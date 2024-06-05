3e6b0dd5736ce34dd11890bc0ab86309dd4c74d2
BurcuMengu <burcumengu@gmail.com>
2024-06-05 15:31:52 +0300

    push -u origin main

---
+++ index.js
@@ -0,0 +1,52 @@
+import express from "express";
+import axios from " axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://api.spotify.com/v1";
+
+//Spotify API Credentials
+
+const clientID = "";
+const clientSecret = "";
+const yourAccessToken = "BQDBKJ5eo5jxbtpWjVOj7ryS84khybFpP_lTqzV7uV-T_m0cTfwvdn5BnBSKPxKgEb11";
+const config = {
+    headers: { Authorization: `Bearer ${yourAccessToken}`},
+};
+
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) =>  {
+    res.render("index.ejs", {content: "Waiting for data..." });
+});
+
+//Get a Track by ID
+
+app.post("/get-track", async (req, res) => {
+    const trackId = req.body.id;
+    try {
+        const result = await axios.get(API_URL + "/tracks/" + trackId, config);
+        res.render("index.ejs", {content: JSON.stringify(result.data) });
+    } catch (error) {
+        res.render("index.ejs", {content: JSON.stringify(error.response.data) });
+    }
+});
+
+//Create a Playlist
+
+app.post("/create-playlist", async (req, res) => {
+    try {
+        const result = await axios.post(API_URL + "/users/" + userId, playlist, req.body, config);
+        res.render("index.ejs", {content: JSON.stringify(result.data) });
+    } catch (error) {
+        res.render("index.ejs", {content: JSON.stringify(error.response.data) });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
